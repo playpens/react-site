4406d3667e450ccec8ddabe69fd6a302c213d6ac
johncokos <runner@fv-az191-776.ggaq42fduhvefhuza3ifwajfkf.bx.internal.cloudapp.net>
2021-07-06 19:58:40 +0000

    Updates

---
+++ precache-manifest.79a1f10e0e9499101c8436617f763842.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0622e421644935f3bfc1ce102b6aa804",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "67c7bfc318d1b3c6cdde",
+    "url": "/react-site/static/js/2.b1c8c03f.chunk.js"
+  },
+  {
+    "revision": "413c5af6568167233f17",
+    "url": "/react-site/static/js/main.1eb248af.chunk.js"
+  },
+  {
+    "revision": "8a033abe2b3da3f09d23",
+    "url": "/react-site/static/js/runtime~main.aa95a05d.js"
+  }
+]);
